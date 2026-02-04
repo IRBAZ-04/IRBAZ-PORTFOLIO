5e3bd902e5f4fe0f8424f3cd333162a5473ef683
IRBAZ-04 <pmirbazraihan@gmail.com>
2026-02-04 23:33:58 +0530

    skills

--- index.js
+++ index.js
@@ -41,3 +41,20 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+function openSkill(evt, skillName) {
+  let i, tabcontent, tabbtns;
+
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].classList.remove("active");
+  }
+
+  tabbtns = document.getElementsByClassName("tab-btn");
+  for (i = 0; i < tabbtns.length; i++) {
+    tabbtns[i].classList.remove("active");
+  }
+
+  document.getElementById(skillName).classList.add("active");
+  evt.currentTarget.classList.add("active");
+}
